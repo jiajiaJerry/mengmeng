afa68d9834c6af9ff4da123ded8363c1f5985c21
Jerry <13632357948@163.com>
2019-05-15 09:47:13 +0800

    Splash界面

--- app/src/main/java/com/jiajia/coco/mengmeng/MainActivity.kt
+++ app/src/main/java/com/jiajia/coco/mengmeng/MainActivity.kt
@@ -1,64 +1,18 @@
 package com.jiajia.coco.mengmeng
 
-import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
+import android.support.v7.app.AppCompatActivity
 
 /**
  * @author Create by Jerry
  * @date on 2019-05-13
  * @description 主页
  */
-class MainActivity : AppCompatActivity() {
-    private val mWebView by lazy {
-        webview
-    }
+class MainActivity : BaseActivity() {
+    override fun getLayoutResId(): Int =R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setWebView()
-    }
-
-    //lambad表达式
-    @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
-    var setWebView = {
-        //1.开启Kotlin与H5通信开关
-        mWebView.settings.javaScriptEnabled=true
-        mWebView.webViewClient = MyWebViewClient()
-        mWebView.webChromeClient=MyWebChromeClient()
-
-        //Kotlin与H5通信方式1:H5调用Kotlin方法
-        //设置Kotlin与H5通信桥梁类
-        mWebView.addJavascriptInterface(JavaScriptMethods(this),"jsInterface")
-
-        //加载网页
-        mWebView.loadUrl("http://192.168.31.34:8080/WEB22/index.html")
-
-
-    }
-
-    private inner class MyWebViewClient : WebViewClient() {
-        override fun onPageFinished(view: WebView?, url: String?) {
-            super.onPageFinished(view, url)
-            //调用H5的方法
-//            mWebView.loadUrl("javascript:方法名(参数)")
-            val json = JSONObject()
-            json.put("name","Kotlin")
-            mWebView.loadUrl("javascript:showMessage($json)")
-        }
-    }
-
-    private class MyWebChromeClient:WebChromeClient(){
-        //加载进度条
-        override fun onProgressChanged(view: WebView?, newProgress: Int) {
-            super.onProgressChanged(view, newProgress)
-        }
     }
 
 }
---
+++ app/src/main/java/com/jiajia/coco/mengmeng/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.jiajia.coco.mengmeng
+
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun getLayoutResId(): Int =R.layout.activity_splash
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
